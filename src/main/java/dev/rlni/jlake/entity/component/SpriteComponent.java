580dc976ec446bbb5e4cba77a27915a23ead5492
2cherrry <160283747+2cherrry@users.noreply.github.com>
2024-03-22 10:11:11 -0400

    Make sprite shader static

--- src/main/java/dev/rlni/jlake/entity/component/SpriteComponent.java
+++ src/main/java/dev/rlni/jlake/entity/component/SpriteComponent.java
@@ -11,11 +11,11 @@ public class SpriteComponent implements EntityComponent, IDrawable {
     private int mLayerHash = 0;
     private Matrix4f mMatrix = new Matrix4f();
     private final Texture mTexture;
-    private final ShaderProgram mShaderProgram;
+
+    private static final ShaderProgram sShaderProgram = new ShaderProgram("jlake/shaders/sprite.vsh", "jlake/shaders/sprite.fsh");
 
     public SpriteComponent(final String texturePath, final String layer, final Texture.FilterMode filterMode) {
         mTexture = new Texture(texturePath, filterMode);
-        mShaderProgram = new ShaderProgram("jlake/shaders/sprite.vsh", "jlake/shaders/sprite.fsh");
         this.setLayer(layer);
     }
 
@@ -38,9 +38,9 @@ public class SpriteComponent implements EntityComponent, IDrawable {
     public void draw(final RenderInfo renderInfo) {
         mTexture.bind(0);
 
-        mShaderProgram.bind();
-        mShaderProgram.setMat4(0, mMatrix);
-        mShaderProgram.setMat4(1, renderInfo.camera().getViewProjection());
+        sShaderProgram.bind();
+        sShaderProgram.setMat4(0, mMatrix);
+        sShaderProgram.setMat4(1, renderInfo.camera().getViewProjection());
 
         GL46.glBindVertexArray(PrimitiveShape.getQuadVAO());
         GL46.glBindBuffer(GL46.GL_ELEMENT_ARRAY_BUFFER, PrimitiveShape.getQuadEBO());
